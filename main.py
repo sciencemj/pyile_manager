834b04c87be93aee8c21425c77b6b2b8eb65c6fd
sciencemj <sciencemj.park@gmail.com>
2026-02-01 01:00:03 +0900

    formatting and load_setting

--- main.py
+++ main.py
@@ -1,39 +1,51 @@
-from urllib.parse import urlparse
-from pathlib import Path
-import shutil
+import json
 import os
+import shutil
 import subprocess
+from pathlib import Path
+from urllib.parse import urlparse
+
+from watchdog.events import DirCreatedEvent, FileCreatedEvent, FileSystemEventHandler
 from watchdog.observers import Observer
-from watchdog.events import FileCreatedEvent, FileSystemEventHandler, DirCreatedEvent
+
+
+def load_setting(path: str) -> dict[str, object]:
+    with open(path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+        return data
+
+
+setting = load_setting("pyile_manager_setting.json")
 
 
 def get_metadata_mdls(path: str):
     try:
-        result = subprocess.run(['mdls', path], capture_output=True, text=True)
+        result = subprocess.run(["mdls", path], capture_output=True, text=True)
     except subprocess.CalledProcessError as e:
         print(f"Error while getting meta data by mdls: {e.stderr}")
     except FileNotFoundError:
         print("File not found")
     else:
         # wherefrom 1st object = download file link, 2nd object = viewing page when download
-        result = result.stdout.strip().split('kMDItemWhereFroms')
+        result = result.stdout.strip().split("kMDItemWhereFroms")
         if len(result) < 2:
             return None
         result = result[1].strip()[1:-2].strip()[1:].strip()
-        result = result.split(',\n')[1].strip()
+        result = result.split(",\n")[1].strip()
         return result.strip('"')
-    
+
+
 def get_domain_name(url: str, loc=1):
     parsed_url = urlparse(url=url)
     domain_subdomain = parsed_url.netloc
-    return domain_subdomain.split('.')[loc]
+    return domain_subdomain.split(".")[loc]
 
 
 class NewFileHandler(FileSystemEventHandler):
     def __init__(self, path) -> None:
         super().__init__()
         self.path = path
-        self.delete_duplicate = True
+        self.remove_duplicate = True
 
     def on_created(self, event: FileCreatedEvent | DirCreatedEvent) -> None:
         if event.is_directory:
@@ -41,14 +53,16 @@ class NewFileHandler(FileSystemEventHandler):
         else:
             src_path = str(event.src_path)
             filename = os.path.basename(src_path)
-            if not filename.endswith(('.crdownload', '.tmp', '.part')):
+            if not filename.endswith((".crdownload", ".tmp", ".part")):
                 data = get_metadata_mdls(src_path)
                 if data:
                     destination = self.path + get_domain_name(url=data, loc=1)
                     Path(destination).mkdir(exist_ok=True)
                     if Path(destination + "/" + filename).is_file():
                         print("File already exist!")
-                        if self.delete_duplicate: os.remove(event.src_path); print(f"New File {filename} deleted!")
+                        if self.remove_duplicate:
+                            os.remove(event.src_path)
+                            print(f"New File {filename} removed!")
                         return
                     shutil.move(src_path, destination + "/" + filename)
                     print(f"file moved! {src_path} --> {destination}")
