8602da5bc6d2ae77cf124dc9b452c2d7a2c9268a
sciencemj <sciencemj.park@gmail.com>
2026-02-03 15:17:59 +0900

    feat: Initialize a new SwiftUI application project with a basic "Hello, world!" view.

---
+++ pyile_manager_gui/pyile_manager/pyile_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pyile_managerApp.swift
+//  pyile_manager
+//
+//  Created by Minjun Park on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct pyile_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
